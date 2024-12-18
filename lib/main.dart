fd4bbfdf3b074236af1fc87d2cb68e79576c6335
Bobonovagulnoza <bobonovagulnoza68@gmail.com>
2024-12-18 10:19:24 +0500

    4 uyish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(const RecipeApp());
 
@@ -7,7 +8,8 @@ class RecipeApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: CategoriesPage(),
     );
   }
@@ -18,10 +20,51 @@ class CategoriesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF1C0F0D),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      backgroundColor: const Color(0xFF1C0F0D),
+      extendBody: true,
+      appBar: AppBar(
+        leadingWidth: 50,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Center(
+            child: SizedBox(
+              width: 21,
+              height: 14,
+              child: SvgPicture.asset(
+                "assets/icons/back-arrow.svg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        title: const Text(
+          "Categories",
+          style: TextStyle(
+            color: Color(0xFFFD5D69),
+          ),
+        ),
+        actions: const [
+          AppBarActionItem(
+            image: "assets/icons/notification.svg",
+            width: 12,
+            height: 17,
+          ),
+          SizedBox(width: 5),
+          AppBarActionItem(
+            image: "assets/icons/search.svg",
+            width: 14,
+            height: 18,
+          ),
+          SizedBox(
+            width: 20,
+          )
+        ],
+        centerTitle: true,
+        backgroundColor: const Color(0xFF1C0F0D),
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           MainCategoryItem(image: "assets/seafood.png", text: "Seafood"),
           SizedBox(height: 16),
@@ -50,6 +93,61 @@ class CategoriesPage extends StatelessWidget {
             ],
           ),
         ],
+      ),
+      bottomNavigationBar: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          width: 281,
+          height: 56,
+          decoration: BoxDecoration(
+            color: const Color(0xFFFD5D69),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SvgPicture.asset("assets/icons/home.svg"),
+              SvgPicture.asset("assets/icons/community.svg"),
+              SvgPicture.asset("assets/icons/categories.svg"),
+              SvgPicture.asset("assets/icons/profile.svg"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AppBarActionItem extends StatelessWidget {
+  const AppBarActionItem({
+    super.key,
+    required this.image,
+    required this.width,
+    required this.height,
+  });
+
+  final String image;
+  final double width, height;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 28,
+      height: 28,
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFC6C9),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Center(
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: SvgPicture.asset(
+            image,
+            fit: BoxFit.cover,
+          ),
+        ),
       ),
     );
   }
